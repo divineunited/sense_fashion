c0b2c02e4b92e1cf5e422d78aaf6dd5f51f64b57
DivineUnited <DanielDukeVu@outlook.com>
2019-05-21 18:07:40 +0800

    handling file uploads

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template, request
 from flask import redirect, url_for, jsonify
+from flask import flash
+from flask import send_from_directory
 import pandas as pd
 import numpy as np
 import os
-from google.cloud import bigquery
+# from google.cloud import bigquery
 import json
+from werkzeug.utils import secure_filename
 
 # custom imports:
 
+
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
 app=Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 # #### QUERY OUR TRANSACTION AND MOVIE DATA FROM GOOGLE BIG QUERY:
@@ -31,54 +42,38 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 # data = query_data.get_data(client) # dataframe of transaction data for our recommendation systems
 
 
-@app.route('/index')
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
-
-
-# when the form press submit, it links it to the redirect which will be sent here and then passes the prediction variable to the results page after back-end recommendation logic completed:
-@app.route('/myredirect', methods = ['POST'])
-def my_redirect():
-    global data
-    global movie_dict
     if request.method == 'POST':
-        # from the request form, convert it to a dictionary saved as this variable
-        _features = request.form.to_dict()
-
-        ### converting inputs to their correct value types:
-        _features['movie_1'] = int(_features['movie_1'])
-        _features['movie_2'] = int(_features['movie_2'])
-        _features['movie_3'] = int(_features['movie_3'])
-        _features['movie_4'] = int(_features['movie_4'])
-
-        # get the values and turn it into a list
-        _features=list(_features.values())
-
-        # get our recommendation movie_ids:
-        movie_ids = recommend_engine.your_item_to_item_recommendations(_features, data)
-
-        # convert them to movie titles:
-        predictions = [movie_dict[id] for id in movie_ids]
-
-        # send it as a proper JSON dumps string for the redirect routing so that it can be unpacked using a JSON loads:
-        predictions = json.dumps(predictions)
-
-        # passing our predictions JSON dump and an achor to the result url.
-        return redirect(url_for('result', predictions = predictions, _anchor='services'))
+        # check if the post request has the file part
+        if 'pic' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['pic']
+        # if user does not select file, browser also submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename) # always use that function to secure a filename before storing it directly on the filesystem.
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) # saving that file onto our server
+            return redirect(url_for('result', filename=filename, _anchor='sense'))
+    else:
+        return render_template("index.html")
 
 
-
-# Wanted to redirect so that it can pass the anchor of where I want to land in the results page (services subsection of page).
-# Thank you to this code to help pass the redirect variable to this result route: 
-# https://stackoverflow.com/questions/26954122/how-can-i-pass-arguments-into-redirecturl-for-of-flask
-@app.route('/result/<predictions>')
-def result(predictions):
-
+# if our index is a POST request, it will save the image, and then redirect to this page and serve up the image.
+@app.route('/result/<filename>')
+def result(filename):
+    # return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    # send it as a proper JSON dumps string for the redirect routing so that it can be unpacked using a JSON loads:
+    # predictions = json.dumps(predictions)
     # taking our passed json dump and loading it back out as a list to pass to our results template
-    predictions = json.loads(predictions)
+    # predictions = json.loads(predictions)
+    image_path = "\\" + os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    return render_template("result.html", image_path = image_path)
 
-    return render_template("result.html", predictions = predictions)
 
 if __name__ == "__main__":
     app.run(debug=True)
